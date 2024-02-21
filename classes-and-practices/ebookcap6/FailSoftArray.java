7dbf745739f7ce049c5be7bf6cc6a43bfebb3ddf
Roberto J <robertonsj31@gmail.com>
2024-02-21 12:37:28 -0300

    Java program from Cap 6 of the ebook - FailSoftArray

---
+++ classes-and-practices/ebookcap6/FailSoftArray.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ebookcap6;
+
+/**
+ *
+ * @author logistica
+ */
+public class FailSoftArray {
+    
+}
